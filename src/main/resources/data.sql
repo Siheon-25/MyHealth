730a0ac8dffcfc05859493eae1f5f990875e844e
LEE Siheon <dltlgjs2022@gmail.com>
2025-03-12 23:54:40 +0900

    더미데이터 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO health(date, content) VALUES('2024','1232')
-INSERT INTO health(date, content) VALUES('2025','dfsa')
-INSERT INTO health(date, content) VALUES('2025','lsmdnfkl')
+INSERT INTO health(date, content) VALUES('2025-03-09','가슴: 벤치 10kg, 체스트 프레스 머신 20kg\n이두: 이두컬 10kg')
+INSERT INTO health(date, content) VALUES('2025-03-10','등: 시티드로우 30kg, 랫풀다운 25kg\n삼두: 오버헤드 익스텐션 10kg')
+INSERT INTO health(date, content) VALUES('2025-03-11','하체: 레그프레스 40kg, 시티드 레그 익스텐션: 30kg\n어깨: 숄더프레스 20kg')
